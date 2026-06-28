7befed06a3c2f808a752d6e63f1965bb6dc05bf7
Samuel Rodrigus <samuelrodrigus@gmail.com>
2026-06-28 05:13:35 -0300

    Enhance comments and clarify save function usage
    Added comments for clarity and updated the save function note.

--- aplicacao.py
+++ aplicacao.py
@@ -11,7 +11,7 @@ from openpyxl import load_workbook
 arquivo = load_workbook("lista_contatos_ficticios.xlsx")
 #ver as abas
 aba_nomes = arquivo["Contatos"]
-#Quantidade de linhas
+#Quantidade de linhas no excel
 linhas = aba_nomes.max_row
 
 nome = aba_nomes.cell(row=1, column=1).value
@@ -26,8 +26,8 @@ for row in aba_nomes.iter_rows(min_row=2, values_only=True):
         print(f"Enviando mensagem para {nome} ({telefone})...")
         
         try:
-            # pywhatkit.sendwhatmsg_instantly(phone_no, message, wait_time, tab_close, close_time)
-            # wait_time: tempo de espera antes de enviar (segundos)
+            # pywhatkit.sendwhatmsg_instantly
+            # Esoerar 15 seundos: tempo de espera antes de enviar (segundos)
             # tab_close: se deve fechar a aba após enviar
             kit.sendwhatmsg_instantly(
                 phone_no=str(telefone),
@@ -36,15 +36,16 @@ for row in aba_nomes.iter_rows(min_row=2, values_only=True):
                 tab_close=True,
                 close_time=3
             )
+            # Mostra se a mensagem foi enviada com sucesso
             print(f"Sucesso ao agendar envio para {nome}!")
             
             # Pequena pausa entre os envios para evitar bloqueios
             time.sleep(5)
             
         except Exception as e:
+            # Mostra se a mensagem não foi enviada  
             print(f"Erro ao enviar para {nome}: {e}")
     
 
-#para salvar o arquivo
-
-arquivo.save("lista_contatos_ficticios.xlsx")
+#para salvar o arquivo caso tenha alguma alteração no sistema
+#arquivo.save("lista_contatos_ficticios.xlsx")
